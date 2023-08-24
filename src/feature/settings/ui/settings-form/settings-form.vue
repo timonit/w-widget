f5be3263e6ed5e6788dc4410a34832db3b6aff10
timon <timonsus@gmail.com>
2023-08-24 13:59:40 +0500

    form settings location name trim and capitalize

--- src/feature/settings/ui/settings-form/settings-form.vue
+++ src/feature/settings/ui/settings-form/settings-form.vue
@@ -3,7 +3,7 @@
     <label class="text-sm text-gray-400">Add location</label>
     <div class="form-control flex flex-nowrap shadow-md">
       <input v-model="city" placeholder="Name of city" type="text" class="w-full rounded-md rounded-r-none border-none" />
-      <button class="rounded-md rounded-l-none px-4 bg-sky-500 text-white" type="button">
+      <button class="rounded-md rounded-l-none px-4 bg-sky-500 text-white" type="submit">
         <font-awesome-icon icon="fa-floppy-disk" />
       </button>
     </div>
@@ -18,7 +18,9 @@ const emit = defineEmits<{(e: 'onSave', value: string): void}>();
 
 const save = (event: Event) => {
   event.preventDefault();
-  emit('onSave', city.value);
+  const rx = new RegExp('([^\\w\u0400-\u04FF]|^)([\\w\u0400-\u04FF])');
+  const value = city.value.trim().toLowerCase();
+  if (value) emit('onSave', value.replace(rx, (match) => match.toUpperCase()));
   city.value = '';
 }
 </script>
